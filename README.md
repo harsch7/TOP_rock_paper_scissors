00ee805d54f6cde21e891c453ce0cd9dfb7ae035
harsch7 <85777545+harsch7@users.noreply.github.com>
2025-01-11 22:04:22 -0800

    Initial commit with skeleton code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TOP_rock_paper_scissors
+
+Mini javascript program for rock, paper, scissors game for The Odin Project - Fundamentals.
---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+function getComputerChoice() 
+{
+    let randNum = Math.floor(Math.random() * 3);
+
+    if (randNum == 0)
+        return "rock";
+    else if (randNum == 1)
+        return "paper";
+    else if (randNum == 2)
+        return "scissors";
+}
+
+console.log(getComputerChoice());
